f49e2bfcb4eaf8d739a9609ba5cb5196e8383bc9
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-07-07 10:28:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 const creatureDiv = document.getElementById('creature');
+const sound = document.getElementById('sound');
 
-const creatures = ['🐉', '🐙', '🦄', '👾', '🦖', '🐬', '🕷️', '🐢', '🦑', '🦀'];
+
+
+const creatures = [
+    { icon: '🐉', class: 'dragon', sound: 'https://www.soundjay.com/nature/dragon-roar-01.mp3' },
+    { icon: '🐙', class: 'octopus', sound: 'https://www.soundjay.com/nature/water-bubble-01.mp3' },
+    { icon: '🦄', class: 'unicorn', sound: 'https://www.soundjay.com/button/beep-07.mp3' },
+    { icon: '👾', class: 'alien', sound: 'https://www.soundjay.com/button/button-09.mp3' },
+    { icon: '🦖', class: 'trex', sound: 'https://www.soundjay.com/nature/dinosaur-roar-01.mp3' },
+    { icon: '🐬', class: 'dolphin', sound: 'https://www.soundjay.com/nature/dolphin-01.mp3' },
+    { icon: '🕷️', class: 'spider', sound: 'https://www.soundjay.com/button/beep-10.mp3' },
+    { icon: '🐢', class: 'turtle', sound: 'https://www.soundjay.com/nature/turtle-swim-01.mp3' },
+    { icon: '🦑', class: 'squid', sound: 'https://www.soundjay.com/nature/water-bubble-02.mp3' },
+    { icon: '🦀', class: 'crab', sound: 'https://www.soundjay.com/nature/crab-01.mp3' }
+];
+
 
 
 function changeCreature() {
     const randomIndex = Math.floor(Math.random() * creatures.length);
+    const creature = creatures[randomIndex];
     creatureDiv.style.opacity = 0;
+    sound.pause();
     setTimeout(() => {
-        creatureDiv.textContent = creatures[randomIndex];
+        creatureDiv.innerHTML = creature.icon;
+        creatureDiv.className = '';
+        creatureDiv.classList.add('creature-animation', creature.class);
         creatureDiv.style.opacity = 1;
+        sound.src = creature.sound;
+        sound.play();
     }, 500);
 }
 
 
 
-setInterval(changeCreature, 2000);
-
+setInterval(changeCreature, 3000);
 
 
 changeCreature();
-    
